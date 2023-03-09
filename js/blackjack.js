924279dd3b85262bb23b1766efe52650cbb6bab0
Hugo SIMON <h.simon@ludus-academie.com>
2023-03-09 10:32:40 +0100

    Add action
    On peut maintenant jouer normalement au jeu avec hit, stand et double, il faudrait juste rajouter split et p-ê assurance.
    Nouvelles variables globales pour le joueur, croupier et le packet.
    Vérifie si on bust ou blackjack.
    Une fonction a part pour l'affichage des cartes

--- js/blackjack.js
+++ js/blackjack.js
@@ -22,16 +22,16 @@ class ProjectInCarte {
     }
 }
 
-let playerCards = [];
+let playerCards = []; // Tableau contenant les cartes du joueur
+let croupierCards = []; // Tableau contenant les cartes du croupier
+let packet = []; // Tableau contenant les cartes du paquet
 
-function AfficheCardsPlayer() {
-    console.log(playerCards);
-}
+let allReady = false; // Booléen indiquant si le jeu est déjà distribué
 
 function createPack() {
     let pack = [];
     let couleurs = ["coeur", "carreau", "pique", "trefle"];
-    let valeurs = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R"];
+    let valeurs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R"];
     for (let i = 0; i < couleurs.length; i++) {
         for (let j = 0; j < valeurs.length; j++) {
             pack.push(new Carte(couleurs[i], valeurs[j]));
@@ -59,24 +59,131 @@ function distributeCards(pack, n) {
     return cards;
 }
 
-function getCardValue(card) {
-    if (card.valeur === "A") {
-        return 11; // A modifier pour le cas où la valeur de l'as est 1
-    } else if (card.valeur === "V" || card.valeur === "D" || card.valeur === "R") {
-        return 10;
-    } else {
-        return parseInt(card.valeur);
+function getCardValue(cards) {
+    let value = 0;
+    let hasAce = false;
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].valeur === "1") {
+            hasAce = true;
+        }
+        if (cards[i].valeur === "V" || cards[i].valeur === "D" || cards[i].valeur === "R") {
+            value += 10;
+        } else {
+            value += parseInt(cards[i].valeur);
+        }
+    }
+    if (hasAce && value < 12) {
+        value += 10;
+    }
+    return value;
+}
+
+function reFillPack() {
+    packet = createPack();
+    packet = shufflePack(packet);
+}
+
+function bust(cards) {
+    return getCardValue(cards) > 21;
+}
+
+function blackjack(cards) {
+    return getCardValue(cards) === 21;
+}
+
+function hit() {
+    if (allReady) {
+        playerCards.push(distributeCards(packet, 1)[0]);
+        afficheCards(playerCards, "joueur");
+        if (bust(playerCards)) {
+            console.log("Vous avez perdu !")
+            allReady = false;
+        } else if (blackjack(playerCards)) {
+            console.log("Blackjack ! Vous avez gagné !")
+            allReady = false;
+        }
     }
 }
 
-function main() {
-    let pack = createPack();
-    pack = shufflePack(pack);
-    playerCards = distributeCards(pack, 2);
-    let score = 0;
-    for (let i = 0; i < playerCards.length; i++) {
-        console.log(playerCards[i].couleur + " " + playerCards[i].valeur);
-        score += getCardValue(playerCards[i]);
+function stand() {
+    if (allReady) {
+        while (getCardValue(croupierCards) < 17) {
+            croupierCards.push(distributeCards(packet, 1)[0]);
+        }
+        if (bust(croupierCards)) {
+            console.log("Le croupier a perdu !")
+        } else if (blackjack(croupierCards)) {
+            console.log("Blackjack ! Le croupier a gagné !")
+        } else if (getCardValue(playerCards) > getCardValue(croupierCards)) {
+            console.log("Vous avez gagné !")
+        } else if (getCardValue(playerCards) < getCardValue(croupierCards)) {
+            console.log("Vous avez perdu !")
+        } else {
+            console.log("Egalité !")
+        }
+        afficheCards(playerCards, "joueur");
+        afficheCards(croupierCards, "croupier");
+        allReady = false;
     }
-    console.log(score);
+}
+
+function double() {
+    if (allReady) {
+        hit();
+        if (allReady) {
+            stand();
+        }
+    }
+}
+
+function afficheCards(cards, etat) {
+    let text = "";
+    if (etat === "joueur") {
+        text = "Vous avez : "
+        for (let i = 0; i < cards.length; i++) {
+            text += cards[i].valeur + " de " + cards[i].couleur + ", ";
+        }
+        text += "pour un total de " + getCardValue(cards) + " points.";
+    }
+    if (etat === "croupier") {
+        text = "Le croupier a : "
+        for (let i = 0; i < cards.length; i++) {
+            text += cards[i].valeur + " de " + cards[i].couleur + ", ";
+        }
+        text += "pour un total de " + getCardValue(cards) + " points.";
+    }
+    if (etat === "cache") {
+        text = "Le croupier a : "
+        text += cards[0].valeur + " de " + cards[0].couleur + " et une carte cachée, avec un score de " + getCardValue([cards[0]]) + " points.";
+    }
+    console.log(text);
+}
+
+function startGame() {
+    if (!allReady) {
+        if (packet.length < 10) {
+            reFillPack();
+        }
+        playerCards = distributeCards(packet, 1);
+        croupierCards = distributeCards(packet, 1);
+        playerCards.push(distributeCards(packet, 1)[0]);
+        croupierCards.push(distributeCards(packet, 1)[0]);
+        allReady = true;
+
+        afficheCards(playerCards, "joueur");
+        afficheCards(croupierCards, "cache");
+
+        if (blackjack(playerCards) && blackjack(croupierCards)) {
+            console.log("Égalité !")
+            allReady = false;
+        } else if (blackjack(playerCards)) {
+            console.log("Blackjack ! Vous avez gagné !")
+            allReady = false;
+        } else if (blackjack(croupierCards)) {
+            console.log("Blackjack ! Le croupier a gagné !")
+            allReady = false;
+        }
+    } else {
+        console.log("La partie est déjà lancée !")
+    }
 }
